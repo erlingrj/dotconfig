184e40e18ac23877e519f0127149bd09d90fa72a
Erling Rennemo Jellum <erling.r.jellum@ntnu.no>
2023-02-23 11:43:07 +0100

    Add git aliases

---
+++ git-aliases.sh
@@ -0,0 +1,8 @@
+git config --global alias.st 'status -sb' 
+git config --global alias.co 'checkout' 
+git config --global alias.su 'submodule update --init --recursive' 
+git config --global alias.b 'checkout -b' 
+git config --global alias.cmu 'commit -m "update"' 
+git config --global alias.cm 'commit -m'
+git config --global alias.ai 'add -i'
+git config --global alias.who 'add shortlog -s -n --all'
